ad51c177db17ea5e242388e2ae36d25daad44d9e
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-08-31 17:24:40 +0600

    Add authApi from previous vite shadcn project

---
+++ src/redux/api/authApi.ts
@@ -0,0 +1,25 @@
+import { baseApi } from "./baseApi";
+
+const AUTH_URL = "/auth";
+
+const authApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (loginData) => ({
+        url: `${AUTH_URL}/login`,
+        method: "POST",
+        body: loginData,
+      }),
+    }),
+
+    signup: builder.mutation({
+      query: (signupData) => ({
+        url: `${AUTH_URL}/signup`,
+        method: "POST",
+        body: signupData,
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useSignupMutation } = authApi;
