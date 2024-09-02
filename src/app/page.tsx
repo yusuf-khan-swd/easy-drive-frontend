fa7789006d101f9c36ce01d4f364db8ca539b7c8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-02 07:58:34 +0600

    Add Navbar in root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
+import Footer from "@/components/Shared/Footer";
+import Navbar from "@/components/Shared/Navbar";
 import { Button, Stack } from "@mui/material";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Stack spacing={2} direction="row">
-        <Link href="/home">
-          <Button variant="contained">Home</Button>
-        </Link>
-        <Link href="/dashboard">
-          <Button variant="outlined">Dashboard</Button>
-        </Link>
-      </Stack>
+      <Navbar />
+      <div className="min-h-screen">
+        <Stack spacing={2} direction="row">
+          <Link href="/home">
+            <Button variant="contained">Home</Button>
+          </Link>
+          <Link href="/dashboard">
+            <Button variant="outlined">Dashboard</Button>
+          </Link>
+        </Stack>
+      </div>
+      <Footer />
     </div>
   );
 }
