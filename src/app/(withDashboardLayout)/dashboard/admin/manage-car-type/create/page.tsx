9df0e4f8cc4b1fd4e069217942e4df876d69ebf9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-02 13:08:14 +0600

    create manage-car-type and create car type

---
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-car-type/create/page.tsx
@@ -0,0 +1,9 @@
+const CreateCarType = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Create Car Type Page</h1>
+    </div>
+  );
+};
+
+export default CreateCarType;
---
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-car-type/page.tsx
@@ -0,0 +1,9 @@
+const ManageCarType = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Manage Car Type Page</h1>
+    </div>
+  );
+};
+
+export default ManageCarType;
