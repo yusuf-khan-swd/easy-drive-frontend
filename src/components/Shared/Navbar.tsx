ec7d0f4942138fcbebe6a5adacc9cc68e202892d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-02 07:58:45 +0600

    Add responsive Navbar

--- src/components/Shared/Navbar.tsx
+++ src/components/Shared/Navbar.tsx
@@ -2,69 +2,208 @@
 
 // import useUserInfo from '@/hooks/useUserInfo';
 // import { logoutUser } from '@/services/actions/logoutUser';
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { MouseEvent, useState } from "react";
 
 const Navbar = () => {
+  const websiteName = "EasyDrive";
   //  const userInfo = useUserInfo();
   const router = useRouter();
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   const handleLogOut = () => {
     console.log("btn logout");
     // logoutUser(router);
   };
 
+  const pages = ["home", "cars", "blog"];
+  const settings = ["profile", "account", "dashboard", "logout"];
+
   return (
-    <Box
-      sx={{
-        bgcolor: "primary.main",
-      }}
-    >
-      <Container>
-        <Stack
-          py={2}
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <Typography variant="h4" component={Link} href="/" fontWeight={600}>
-            P
-            <Box component="span" color="#ffffff">
-              H
-            </Box>{" "}
-            Health Care
-          </Typography>
-
-          <Stack direction="row" justifyContent="space-between" gap={4}>
-            <Typography component={Link} href="/consultation" color="#ffffff">
-              Consultation
+    <div>
+      <AppBar position="static">
+        <Container>
+          <Toolbar disableGutters>
+            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link}
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              {websiteName}
             </Typography>
 
-            <Typography color="#ffffff">Diagnostics</Typography>
-            <Typography component={Link} href="/doctors" color="#ffffff">
-              Doctors
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{ display: { xs: "block", md: "none" } }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography
+                      component={Link}
+                      href={page}
+                      sx={{ textAlign: "center" }}
+                    >
+                      <span className="uppercase">{page}</span>
+                    </Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              {websiteName}
             </Typography>
 
-            {/* {userInfo?.userId ? ( */}
-            <Typography component={Link} href="/dashboard" color="#ffffff">
-              Dashboard
-            </Typography>
-            {/* ) : null} */}
-          </Stack>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Link key={page} href={page}>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ m: 2, color: "white", display: "block" }}
+                  >
+                    {page}
+                  </Button>
+                </Link>
+              ))}
+            </Box>
 
-          {/* {userInfo?.userId ? ( */}
-          <Button color="error" onClick={handleLogOut} sx={{ boxShadow: 0 }}>
-            Logout
-          </Button>
-          {/* ) : ( */}
-          <Button component={Link} href="/login">
-            Login
-          </Button>
-          {/* )} */}
-        </Stack>
-      </Container>
-    </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Button component={Link} href="/login">
+                Login
+              </Button>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    {setting === "logout" ? (
+                      <Button
+                        key="logout"
+                        color="error"
+                        onClick={handleLogOut}
+                        sx={{ boxShadow: 0 }}
+                      >
+                        Logout
+                      </Button>
+                    ) : (
+                      <Typography
+                        component={Link}
+                        href={setting}
+                        sx={{ textAlign: "center" }}
+                      >
+                        <span className="uppercase">{setting}</span>
+                      </Typography>
+                    )}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
   );
 };
 
