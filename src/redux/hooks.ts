ed839f8655ddf76a5c37399a4ee782a59c498734
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-03 12:18:05 +0600

    add useDebounced hooks

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import type { AppDispatch, RootState } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+interface IDebounced {
+  query: string;
+  delay: number;
+}
+
+export const useDebounced = ({ query, delay }: IDebounced) => {
+  const [debouncedValue, setDebouncedValue] = useState<string>(query);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(query);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [query, delay]);
+
+  return debouncedValue;
+};
