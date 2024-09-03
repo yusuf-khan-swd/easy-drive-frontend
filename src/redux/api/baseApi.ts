1a87ac12f052a645627039539667687ec23e7701
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-03 22:37:06 +0600

    for baseQuery in redux use getBaseUrl function

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,9 +1,10 @@
+import { getBaseUrl } from "@/helpers/getBaseUrl";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
   endpoints: () => ({}),
   tagTypes: ["car", "booking", "user"],
 });
