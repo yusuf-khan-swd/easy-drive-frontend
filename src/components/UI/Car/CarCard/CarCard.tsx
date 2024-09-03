0e2f893e83a4f34fa156dbee1ee4e994b4c04005
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-03 22:26:50 +0600

    Init CarCard and CarDetailsPage component

---
+++ src/components/UI/Car/CarCard/CarCard.tsx
@@ -0,0 +1,9 @@
+const CarCard = () => {
+  return (
+    <div>
+      <h1>Car Card</h1>
+    </div>
+  );
+};
+
+export default CarCard;
---
+++ src/components/UI/Car/CarDetailsPage/CarDetailsPage.tsx
@@ -0,0 +1,9 @@
+const CarDetailsPage = () => {
+  return (
+    <div>
+      <h1>Car Details Page</h1>
+    </div>
+  );
+};
+
+export default CarDetailsPage;
