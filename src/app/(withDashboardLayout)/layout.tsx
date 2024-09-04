0b89b62f0beddeab8e7b52551f3e868d7f4d7fef
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-04 22:35:41 +0600

    if user is not logged in but try to access dashboard route then redirect user to login page

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -1,12 +1,16 @@
 "use client";
+
 import DashboardDrawer from "@/components/Dashboard/DashboardDrawer";
-// import { isLoggedIn } from '@/services/auth.services';
+import { isLoggedIn } from "@/services/auth.service";
+import { useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  //  const router = useRouter();
-  //  if (!isLoggedIn()) {
-  //     return router.push('/login');
-  //  }
+  const router = useRouter();
+
+  if (!isLoggedIn()) {
+    return router.push("/login");
+  }
+
   return <DashboardDrawer>{children}</DashboardDrawer>;
 };
 
