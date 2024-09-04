40c1bf1cf324f4821302cc98cbcf66d69f3e8e0a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-04 11:50:44 +0600

    add car type

---
+++ src/types/car.ts
@@ -0,0 +1,10 @@
+export type TCar = {
+  _id: string;
+  name: string;
+  description: string;
+  color: string;
+  status: string;
+  isElectric: boolean;
+  features: string[];
+  pricePerHour: number;
+};
