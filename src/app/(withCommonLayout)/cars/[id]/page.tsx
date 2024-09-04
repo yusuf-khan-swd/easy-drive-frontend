467fc8ce2be13b12b7fc5d351d7eab1a4191c5a5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-04 12:10:18 +0600

    [done] CarDetails

--- src/app/(withCommonLayout)/cars/[id]/page.tsx
+++ src/app/(withCommonLayout)/cars/[id]/page.tsx
@@ -1,9 +1,26 @@
+"use client";
+
+import LoadingSpinner from "@/components/Shared/LoadingSpinner";
+import CarCard from "@/components/UI/Car/CarCard/CarCard";
+import { useGetSingleCarQuery } from "@/redux/api/carApi";
+
 const CarDetails = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
+  const { data, isLoading } = useGetSingleCarQuery(id || "");
+  const car = data?.data;
+
+  if (isLoading) return <LoadingSpinner />;
 
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Car Details Page id: {id}</h1>
+    <div className="mb-16 mt-4">
+      <h2 className="text-2xl font-bold text-center mb-6">Car Details</h2>
+      {car ? (
+        <div className="grid grid-cols-1 gap-8">
+          <CarCard car={car} detailsPage />
+        </div>
+      ) : (
+        <h3 className="text-2xl font-bold text-center">No data available</h3>
+      )}
     </div>
   );
 };
