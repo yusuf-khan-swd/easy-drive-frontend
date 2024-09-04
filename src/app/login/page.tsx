0e6c5d76314e554241d78a28991f2b7ac5719d9f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-04 12:37:18 +0600

    [Done] Login page design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,155 @@
-const Login = () => {
+"use client";
+
+import assets from "@/assets";
+import EasyDriveForm from "@/components/Forms/EasyDriveForm";
+import EasyDriveInput from "@/components/Forms/EasyDriveInput";
+// import { userLogin } from "@/services/actions/userLogin";
+// import { storeUserInfo } from "@/services/auth.services";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { FieldValues } from "react-hook-form";
+import { z } from "zod";
+
+export const validationSchema = z.object({
+  email: z.string().email("Please enter a valid email address!"),
+  password: z.string().min(6, "Must be at least 6 characters"),
+});
+
+const LoginPage = () => {
+  const [error, setError] = useState("");
+
+  const handleLogin = async (values: FieldValues) => {
+    console.log(values);
+    try {
+      // const res = await userLogin(values);
+      // if (res?.data?.accessTokeLn) {
+      //   toast.success(res?.message);
+      //   storeUserInfo({ accessToken: res?.data?.accessToken });
+      //   // router.push("/dashboard");
+      // } else {
+      //   setError(res.message);
+      //   // console.log(res);
+      // }
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Login Page</h1>
-    </div>
+    <Container>
+      <Stack
+        sx={{
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
+          }}
+        >
+          <Stack
+            sx={{
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box>
+              <Image src={assets.svgs.logo} width={50} height={50} alt="logo" />
+            </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Login PH HealthCare
+              </Typography>
+            </Box>
+          </Stack>
+
+          {error && (
+            <Box>
+              <Typography
+                sx={{
+                  backgroundColor: "red",
+                  padding: "1px",
+                  borderRadius: "2px",
+                  color: "white",
+                  marginTop: "5px",
+                }}
+              >
+                {error}
+              </Typography>
+            </Box>
+          )}
+
+          <Box>
+            <EasyDriveForm
+              onSubmit={handleLogin}
+              resolver={zodResolver(validationSchema)}
+              defaultValues={{
+                email: "",
+                password: "",
+              }}
+            >
+              <Grid container spacing={2} my={1}>
+                <Grid item md={6}>
+                  <EasyDriveInput
+                    name="email"
+                    label="Email"
+                    type="email"
+                    fullWidth={true}
+                  />
+                </Grid>
+                <Grid item md={6}>
+                  <EasyDriveInput
+                    name="password"
+                    label="Password"
+                    type="password"
+                    fullWidth={true}
+                  />
+                </Grid>
+              </Grid>
+
+              <Link href={"/forgot-password"}>
+                <Typography
+                  mb={1}
+                  textAlign="end"
+                  component="p"
+                  fontWeight={300}
+                  sx={{
+                    textDecoration: "underline",
+                  }}
+                >
+                  Forgot Password?
+                </Typography>
+              </Link>
+
+              <Button
+                sx={{
+                  margin: "10px 0px",
+                }}
+                fullWidth={true}
+                type="submit"
+              >
+                Login
+              </Button>
+              <Typography component="p" fontWeight={300}>
+                Don&apos;t have an account?{" "}
+                <Link href="/register">Create an account</Link>
+              </Typography>
+            </EasyDriveForm>
+          </Box>
+        </Box>
+      </Stack>
+    </Container>
   );
 };
 
-export default Login;
+export default LoginPage;
