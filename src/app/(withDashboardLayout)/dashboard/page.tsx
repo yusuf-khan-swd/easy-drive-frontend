a407e57a9fbdc743555e81985199eab89e40cc13
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-04 23:26:56 +0600

    [fixed] issue of not displaying sidebar and dashboard header because of removing DashboardDrawer from layout so have to add DashboardDrawer as wrapper for dashboard route

--- src/app/(withDashboardLayout)/dashboard/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -1,10 +1,13 @@
+import DashboardDrawer from "@/components/Dashboard/DashboardDrawer";
 import ProfilePage from "@/components/Dashboard/ProfilePage/ProfilePage";
 
 const DashboardHome = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Welcome to dashboard</h1>
-      <ProfilePage />
+      <DashboardDrawer>
+        <h1 className="text-4xl font-bold">Welcome to dashboard</h1>
+        <ProfilePage />
+      </DashboardDrawer>
     </div>
   );
 };
