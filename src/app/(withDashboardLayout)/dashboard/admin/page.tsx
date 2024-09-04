01a7fed8518dea998512be2cd5081fe55a12ab14
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-04 23:12:17 +0600

    use ProfilePage component in Admin and User page

--- src/app/(withDashboardLayout)/dashboard/admin/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/page.tsx
@@ -1,7 +1,10 @@
+import ProfilePage from "@/components/Dashboard/ProfilePage/ProfilePage";
+
 const Admin = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold">Admin Page</h1>
+      <ProfilePage />
     </div>
   );
 };
--- src/app/(withDashboardLayout)/dashboard/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import ProfilePage from "@/components/Dashboard/ProfilePage/ProfilePage";
+
 const DashboardHome = () => {
   return (
     <div>
       <h1 className="text-4xl font-bold">Welcome to dashboard</h1>
+      <ProfilePage />
     </div>
   );
 };
