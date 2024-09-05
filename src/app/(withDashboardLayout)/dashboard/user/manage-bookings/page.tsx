695ce0be6313f19010786b6c3cfe06646178a556
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-05 21:50:57 +0600

    Add User MangeBookings table page

--- src/app/(withDashboardLayout)/dashboard/user/manage-bookings/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-bookings/page.tsx
@@ -1,8 +1,124 @@
+"use client";
+
+import {
+  useDeleteBookingMutation,
+  useMyBookingQuery,
+} from "@/redux/api/bookingApi";
+import { useDebounced } from "@/redux/hooks";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, IconButton, Stack, TextField } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
+
 const ManageBookings = () => {
+  const query: Record<string, any> = {};
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  // console.log(searchTerm);
+
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query["searchTerm"] = searchTerm;
+  }
+
+  const { data, isLoading, isError } = useMyBookingQuery(undefined);
+  const [deleteBooking] = useDeleteBookingMutation();
+
+  const bookings = data?.data;
+  console.log(bookings);
+
+  const handleDelete = async (id: string) => {
+    try {
+      const result = await deleteBooking(id).unwrap();
+      toast.success(result?.message || "Booking deleted Successfully");
+    } catch (error: any) {
+      console.log("Error: ", error);
+      toast.error(error?.data?.message || "Booking delete failed");
+    }
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "user",
+      headerName: "User",
+      flex: 1,
+      valueGetter: (value: any) => value?.email,
+    },
+    {
+      field: "car",
+      headerName: "Car",
+      flex: 1,
+      valueGetter: (value: any) => value?.name,
+    },
+    {
+      field: "car.status",
+      headerName: "Status",
+      flex: 1,
+      renderCell: ({ row }) => row.car.status,
+    },
+    { field: "date", headerName: "Date", flex: 1 },
+    { field: "startTime", headerName: "StartTime", flex: 1 },
+    { field: "endTime", headerName: "EndTime", flex: 1 },
+    { field: "totalCost", headerName: "TotalCost", flex: 1 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 2,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        console.log({ row });
+        return (
+          <Box>
+            <IconButton
+              onClick={() => handleDelete(row._id)}
+              aria-label="delete"
+            >
+              <DeleteIcon sx={{ color: "red" }} />
+            </IconButton>
+            <Link href={`/dashboard/admin/manage-bookings/edit/${row._id}`}>
+              <IconButton aria-label="edit">
+                <EditIcon />
+              </IconButton>
+            </Link>
+          </Box>
+        );
+      },
+    },
+  ];
+
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Manage Booking Page</h1>
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <div></div>
+        <TextField
+          onChange={(e) => setSearchTerm(e.target.value)}
+          size="small"
+          placeholder="search bookings"
+        />
+      </Stack>
+
+      <Box sx={{ my: 2, minWidth: "840px" }}>
+        <DataGrid
+          rows={bookings}
+          columns={columns}
+          getRowId={(row) => row._id}
+          loading={isLoading}
+          slotProps={{
+            loadingOverlay: {
+              variant: "linear-progress",
+              noRowsVariant: "skeleton",
+            },
+          }}
+        />
+      </Box>
+    </Box>
   );
 };
 
