a391a5001abd038af9b7d3a32329b2505bcda961
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-05 12:41:50 +0600

    comment out console log

--- src/components/Dashboard/SidebarItems.tsx
+++ src/components/Dashboard/SidebarItems.tsx
@@ -16,7 +16,7 @@ const SidebarItem = ({ item }: IProps) => {
   const linkPath = `/dashboard/${item.path}`;
   const pathname = usePathname();
 
-  console.log({ pathname, linkPath });
+  // console.log({ pathname, linkPath });
   return (
     <Link href={linkPath}>
       <ListItem
