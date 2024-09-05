84d1eaabc7dda07e2a647adf3b71011749711f01
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-05 13:41:20 +0600

    init update user page

---
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-users/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+const UpdateUser = ({ params }: { params: { id: string } }) => {
+  const id = params?.id;
+
+  return (
+    <div>
+      <h1 className="text-3xl text-center">Update user page id: {}</h1>
+    </div>
+  );
+};
+
+export default UpdateUser;
