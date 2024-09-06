622327b070309f9a7928581d96fa9b7a6da95720
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-06 16:45:38 +0600

    init /profile route page for both user and admin

---
+++ src/app/(withDashboardLayout)/dashboard/user/profile/page.tsx
@@ -0,0 +1,12 @@
+import ProfilePage from "@/components/Dashboard/ProfilePage/ProfilePage";
+
+const UserProfile = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">User Profile Page</h1>
+      <ProfilePage />
+    </div>
+  );
+};
+
+export default UserProfile;
