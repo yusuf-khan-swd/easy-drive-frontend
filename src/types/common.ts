eb52ca6a3edf02a8e6779c2b768939ed3cd65bac
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-06 22:23:26 +0600

    add IMeta type

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,6 +2,12 @@ import { USER_ROLE } from "@/constants/role";
 import { SvgIconTypeMap } from "@mui/material";
 import { OverridableComponent } from "@mui/material/OverridableComponent";
 
+export interface IMeta {
+  page: number;
+  limit: number;
+  total: number;
+}
+
 export type TUserRole = keyof typeof USER_ROLE;
 
 export interface DrawerItem {
