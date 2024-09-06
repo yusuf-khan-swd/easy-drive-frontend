f423da4c99be2f950d12966cdb35937be26bf23f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-06 10:41:37 +0600

    Add HeroSection, FeaturedCars, WhyUs, and Testimonial

--- src/app/(withCommonLayout)/home/page.tsx
+++ src/app/(withCommonLayout)/home/page.tsx
@@ -1,7 +1,19 @@
+import FeaturedCars from "@/components/UI/HomePage/FeaturedCars/FeaturedCars";
+import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
+import Testimonial from "@/components/UI/HomePage/Testimonial/Testimonial";
+import WhyUs from "@/components/UI/HomePage/WhyUs/WhyUs";
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Home Page</h1>
+      <div>
+        <HeroSection />
+      </div>
+      <div>
+        <FeaturedCars />
+        <WhyUs />
+        <Testimonial />
+      </div>
     </div>
   );
 };
