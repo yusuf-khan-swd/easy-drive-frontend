e8c1904e96d81bbeb462c7b1384469e6b2fbec94
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-08 00:33:15 +0600

    use axiosBaseQuery for redux baseQuery

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,10 +1,12 @@
+import { axiosBaseQuery } from "@/helpers/axiosBaseQuery";
 import { getBaseUrl } from "@/helpers/getBaseUrl";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
+  // baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
+  baseQuery: axiosBaseQuery({ baseUrl: getBaseUrl() }),
   endpoints: () => ({}),
   tagTypes: ["car", "booking", "user"],
 });
