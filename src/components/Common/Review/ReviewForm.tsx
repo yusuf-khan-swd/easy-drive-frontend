cddf30a6c9864b9d324c0fa462165aac3b2002ef
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-08 23:42:39 +0600

    [Done] ReviewForm with review added to db

--- src/components/Common/Review/ReviewForm.tsx
+++ src/components/Common/Review/ReviewForm.tsx
@@ -1,210 +1,129 @@
 "use client";
 
-import logo from "@/assets/logo.png";
 import EasyDriveForm from "@/components/Forms/EasyDriveForm";
 import EasyDriveInput from "@/components/Forms/EasyDriveInput";
-import { useLoginMutation } from "@/redux/api/authApi";
-import { storeUserInfo } from "@/services/auth.service";
+import EasyDriveSelectField from "@/components/Forms/EasyDriveSelectField";
+import { ratingOptions } from "@/constants/global";
+import { useCreateReviewMutation } from "@/redux/api/reviewApi";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
 
 export const validationSchema = z.object({
-  email: z.string().email("Please enter a valid email address!"),
-  password: z.string().min(6, "Must be at least 6 characters"),
+  title: z.string().min(1, "Title is required"),
+  description: z.string().min(1, "Description is required"),
+  rating: z.string().min(1, "Rating is required"),
 });
 
-//  TODO: For Login and register try to use server action because it is more secure
+const ReviewForm = ({ carId }: { carId: string }) => {
+  const [review] = useCreateReviewMutation();
 
-const ReviewForm = () => {
-  const [error, setError] = useState("");
-  const [defaultValues, setDefaultValues] = useState({
-    email: "user@easy.com",
-    password: "Pa$$w0rd!",
-  });
-
-  const [login] = useLoginMutation();
-  const router = useRouter();
-
-  const handleLogin = async (values: FieldValues) => {
+  const handleReviewSubmit = async (values: FieldValues) => {
     // console.log(values);
     try {
-      const result = await login(values).unwrap();
+      const reviewData = {
+        ...values,
+        car: carId,
+        rating: Number(values?.rating),
+      };
+
+      const result = await review(reviewData).unwrap();
       console.log({ result });
-      if (result?.data?.token) {
-        storeUserInfo({ accessToken: result?.data?.token });
-        toast.success(result?.data?.message || "Login Success!!");
-        router.push("/dashboard");
+      if (result?.message) {
+        toast.success(result?.message || "Review Submit Success!!");
+      } else {
+        toast.error("Review Submit failed");
       }
     } catch (error: any) {
       console.log("Error: ", error);
-      setError(error?.data?.message);
-      toast.error(error?.data?.message || "login failed");
+      toast.error(error?.data?.message || "Review Submit failed");
     }
-    // ! Below code is for axiosBaseQuery
-    // try {
-    //   const result = await login(values).unwrap();
-    //   console.log(result);
-    //   if (result?.token) {
-    //     toast.success(result?.message || "Login Success!!");
-    //     storeUserInfo({ accessToken: result?.token });
-    //     router.push("/dashboard");
-    //   } else {
-    //     toast.error(result?.message || "login failed");
-    //   }
-    // } catch (error: any) {
-    //   setError(error?.message);
-    //   toast.error(error?.message || "login failed");
-    //   console.log("Error: ", error);
-    // }
   };
 
   return (
-    <Container>
-      <Stack
+    <Stack
+      sx={{
+        marginTop: 4,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
         sx={{
-          height: "100vh",
-          justifyContent: "center",
-          alignItems: "center",
+          maxWidth: 600,
+          width: "100%",
+          boxShadow: 1,
+          borderRadius: 1,
+          p: 4,
+          // textAlign: "center",
         }}
       >
-        <Box
+        <Stack
           sx={{
-            maxWidth: 600,
-            width: "100%",
-            boxShadow: 1,
-            borderRadius: 1,
-            p: 4,
-            textAlign: "center",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <Stack
-            sx={{
-              justifyContent: "center",
-              alignItems: "center",
+          <Box>
+            <Typography variant="h6" fontWeight={600}>
+              Add Review
+            </Typography>
+          </Box>
+        </Stack>
+
+        <Box>
+          <EasyDriveForm
+            onSubmit={handleReviewSubmit}
+            resolver={zodResolver(validationSchema)}
+            defaultValues={{
+              title: "",
+              description: "",
+              rating: "",
             }}
           >
-            <Box>
-              <Link href="/">
-                <Image src={logo} width={50} height={50} alt="logo" />
-              </Link>
-            </Box>
-            <Box>
-              <Link href="/">
-                <Typography variant="h6" fontWeight={600}>
-                  Login Easy Drive
-                </Typography>
-              </Link>
-            </Box>
-          </Stack>
-
-          {error && (
-            <Box>
-              <Typography
-                sx={{
-                  backgroundColor: "red",
-                  padding: "1px",
-                  borderRadius: "2px",
-                  color: "white",
-                  marginTop: "5px",
-                }}
-              >
-                {error}
-              </Typography>
-            </Box>
-          )}
-
-          <Box>
-            <EasyDriveForm
-              onSubmit={handleLogin}
-              resolver={zodResolver(validationSchema)}
-              defaultValues={defaultValues}
-            >
-              <Grid container spacing={2} my={1}>
-                <Grid size={{ xs: 12, md: 6 }}>
-                  <EasyDriveInput
-                    name="email"
-                    label="Email"
-                    type="email"
-                    fullWidth={true}
-                  />
-                </Grid>
-                <Grid size={{ xs: 12, md: 6 }}>
-                  <EasyDriveInput
-                    name="password"
-                    label="Password"
-                    type="password"
-                    fullWidth={true}
-                  />
-                </Grid>
+            <Grid container spacing={2} my={1}>
+              <Grid size={{ xs: 12 }}>
+                <EasyDriveInput
+                  name="title"
+                  label="Title"
+                  type="title"
+                  fullWidth={true}
+                />
               </Grid>
+              <Grid size={{ xs: 12 }}>
+                <EasyDriveInput
+                  name="description"
+                  label="Description"
+                  type="description"
+                  fullWidth={true}
+                />
+              </Grid>
+              <Grid size={{ xs: 12 }}>
+                <EasyDriveSelectField
+                  items={ratingOptions}
+                  name="rating"
+                  label="Rating"
+                  fullWidth={true}
+                />
+              </Grid>
+            </Grid>
 
-              <Link href={"/forgot-password"}>
-                <Typography
-                  mb={1}
-                  textAlign="end"
-                  component="p"
-                  fontWeight={300}
-                  sx={{
-                    textDecoration: "underline",
-                  }}
-                >
-                  Forgot Password?
-                </Typography>
-              </Link>
-
-              <Button
-                sx={{
-                  margin: "10px 0px",
-                }}
-                fullWidth={true}
-                type="submit"
-              >
-                Login
-              </Button>
-              <Typography component="p" fontWeight={300}>
-                Don&apos;t have an account?{" "}
-                <Link href="/register">Create an account</Link>
-              </Typography>
-              <div className="mt-4 text-sm text-center">
-                <Link href="/privacy" className="text-gray-600 underline">
-                  Privacy Policy
-                </Link>{" "}
-                |
-                <Link href="/tos" className="text-gray-600 underline ml-2">
-                  Terms of Service
-                </Link>
-              </div>
-            </EasyDriveForm>
-          </Box>
+            <Button
+              sx={{
+                margin: "10px 0px",
+              }}
+              fullWidth={true}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </EasyDriveForm>
         </Box>
-        <div className="text-center mt-8 space-y-3">
-          <div>
-            <p>
-              User Email: <span className="text-blue-600">user@easy.com</span>
-            </p>
-            <p>
-              User Password: <span className="text-blue-600">Pa$$w0rd!</span>
-            </p>
-          </div>
-          <div>
-            <p>
-              Admin Email: <span className="text-blue-600">admin@easy.com</span>
-            </p>
-            <p>
-              Admin Password: <span className="text-blue-600">Pa$$w0rd!</span>
-            </p>
-          </div>
-        </div>
-      </Stack>
-    </Container>
+      </Box>
+    </Stack>
   );
 };
 
