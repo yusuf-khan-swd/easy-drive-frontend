fc3cb74970a730cb21c9e4afa6f97316b269c569
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-09 10:33:10 +0600

    load user info using profile api

--- src/components/Dashboard/ProfilePage/ProfilePage.tsx
+++ src/components/Dashboard/ProfilePage/ProfilePage.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import LoadingSpinner from "@/components/Shared/LoadingSpinner";
+import { useGetUserProfileQuery } from "@/redux/api/profileApi";
+import { getUserInfo } from "@/services/auth.service";
+
 const ProfilePage = () => {
+  const { userId } = getUserInfo();
+
+  const { data, isLoading } = useGetUserProfileQuery(userId);
+  const user = data?.data;
+
+  console.log(data);
+
+  if (isLoading) return <LoadingSpinner />;
+
   return (
     <div>
       <h1>Profile Page</h1>
