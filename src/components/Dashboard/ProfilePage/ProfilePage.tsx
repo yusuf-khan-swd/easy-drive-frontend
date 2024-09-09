80ded4c0967b947215f68d698105474ce341f86d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-09 10:45:12 +0600

    remove container wrapper from ProfilePage and height 100vh

--- src/components/Dashboard/ProfilePage/ProfilePage.tsx
+++ src/components/Dashboard/ProfilePage/ProfilePage.tsx
@@ -3,7 +3,7 @@ import logo from "@/assets/logo.png";
 import EasyDriveForm from "@/components/Forms/EasyDriveForm";
 import EasyDriveInput from "@/components/Forms/EasyDriveInput";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import Image from "next/image";
 import Link from "next/link";
@@ -62,100 +62,96 @@ const ProfilePage = () => {
 
   return (
     <div>
-      <h1>Profile Page</h1>
-      <Container>
-        <Stack
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box
           sx={{
-            height: "100vh",
-            justifyContent: "center",
-            alignItems: "center",
+            maxWidth: 600,
+            width: "100%",
+            boxShadow: 1,
+            borderRadius: 1,
+            p: 4,
+            textAlign: "center",
           }}
         >
-          <Box
+          <Stack
             sx={{
-              maxWidth: 600,
-              width: "100%",
-              boxShadow: 1,
-              borderRadius: 1,
-              p: 4,
-              textAlign: "center",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <Stack
-              sx={{
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Box>
-                <Link href="/">
-                  <Image src={logo} width={50} height={50} alt="logo" />
-                </Link>
-              </Box>
-              <Box>
-                <Typography variant="h6" fontWeight={600}>
-                  Profile Page
-                </Typography>
-              </Box>
-            </Stack>
-
             <Box>
-              <EasyDriveForm
-                onSubmit={handleSubmit}
-                resolver={zodResolver(validationSchema)}
-                defaultValues={defaultValues}
-              >
-                <Grid container spacing={2} my={1}>
-                  <Grid size={{ xs: 12, md: 12 }}>
-                    <EasyDriveInput label="Name" fullWidth={true} name="name" />
-                  </Grid>
-                  <Grid size={{ xs: 12, md: 6 }}>
-                    <EasyDriveInput
-                      label="Email"
-                      type="email"
-                      fullWidth={true}
-                      name="email"
-                    />
-                  </Grid>
-                  <Grid size={{ xs: 12, md: 6 }}>
-                    <EasyDriveInput
-                      label="Password"
-                      type="password"
-                      fullWidth={true}
-                      name="password"
-                    />
-                  </Grid>
-                  <Grid size={{ xs: 12, md: 6 }}>
-                    <EasyDriveInput
-                      label="Contact Number"
-                      type="tel"
-                      fullWidth={true}
-                      name="phone"
-                    />
-                  </Grid>
-                  <Grid size={{ xs: 12, md: 6 }}>
-                    <EasyDriveInput
-                      label="Address"
-                      fullWidth={true}
-                      name="address"
-                    />
-                  </Grid>
-                </Grid>
-
-                <Button
-                  sx={{
-                    margin: "10px 0px",
-                  }}
-                  fullWidth={true}
-                  type="submit"
-                >
-                  Update
-                </Button>
-              </EasyDriveForm>
+              <Link href="/">
+                <Image src={logo} width={50} height={50} alt="logo" />
+              </Link>
             </Box>
+            <Box>
+              <Typography variant="h6" fontWeight={600}>
+                Profile Page
+              </Typography>
+            </Box>
+          </Stack>
+
+          <Box>
+            <EasyDriveForm
+              onSubmit={handleSubmit}
+              resolver={zodResolver(validationSchema)}
+              defaultValues={defaultValues}
+            >
+              <Grid container spacing={2} my={1}>
+                <Grid size={{ xs: 12, md: 12 }}>
+                  <EasyDriveInput label="Name" fullWidth={true} name="name" />
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                  <EasyDriveInput
+                    label="Email"
+                    type="email"
+                    fullWidth={true}
+                    name="email"
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                  <EasyDriveInput
+                    label="Password"
+                    type="password"
+                    fullWidth={true}
+                    name="password"
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                  <EasyDriveInput
+                    label="Contact Number"
+                    type="tel"
+                    fullWidth={true}
+                    name="phone"
+                  />
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                  <EasyDriveInput
+                    label="Address"
+                    fullWidth={true}
+                    name="address"
+                  />
+                </Grid>
+              </Grid>
+
+              <Button
+                sx={{
+                  margin: "10px 0px",
+                }}
+                fullWidth={true}
+                type="submit"
+              >
+                Update
+              </Button>
+            </EasyDriveForm>
           </Box>
-        </Stack>
-      </Container>
+        </Box>
+      </Stack>
     </div>
   );
 };
