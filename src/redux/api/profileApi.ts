809a655d04da515e8c4f2cff6b859d7586e82950
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-09-09 10:27:54 +0600

    add profileApi slice

---
+++ src/redux/api/profileApi.ts
@@ -0,0 +1,34 @@
+import { authKey } from "@/constants/authKey";
+import { baseApi } from "./baseApi";
+
+const PROFILE_URL = "/profile";
+
+const profileApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getUserProfile: builder.query({
+      query: (id) => ({
+        url: `${PROFILE_URL}/${id}`,
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(authKey)}`,
+        },
+      }),
+      providesTags: ["user"],
+    }),
+
+    updateUserProfile: builder.mutation({
+      query: (userData) => ({
+        url: `${PROFILE_URL}/${userData._id}`,
+        method: "PATCH",
+        body: userData,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem(authKey)}`,
+        },
+      }),
+      invalidatesTags: ["user"],
+    }),
+  }),
+});
+
+export const { useGetUserProfileQuery, useUpdateUserProfileMutation } =
+  profileApi;
