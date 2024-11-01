9ef41ba30ddaa4d73205b28c7a30d7450c3ee0eb
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-11-01 10:00:17 +0600

    [Worked] use body to send login data

--- src/redux/api/authApi.ts
+++ src/redux/api/authApi.ts
@@ -8,8 +8,8 @@ const authApi = baseApi.injectEndpoints({
       query: (loginData) => ({
         url: `${AUTH_URL}/login`,
         method: "POST",
-        data: loginData, // ! Need use data property instead of body property for sending post data in server because of axios
-        // body: loginData, // ? For redux use body property to send post data
+        // data: loginData, // ! Need use data property instead of body property for sending post data in server because of axios
+        body: loginData, // ? For redux use body property to send post data
       }),
     }),
 
