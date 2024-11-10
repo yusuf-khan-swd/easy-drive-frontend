c1ee078056b124f7700a1ea572454c1cc016080b
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-11-10 10:19:46 +0600

    update error response

--- src/helpers/axiosInstance.ts
+++ src/helpers/axiosInstance.ts
@@ -68,7 +68,12 @@ instance.interceptors.response.use(
     //   return responseObject;
     // }
 
-    return Promise.reject(error);
+    return Promise.reject({
+      statusCode: error?.response?.data?.statusCode || 500,
+      message:
+        error?.response?.data?.message || error?.data || "Something went wrong",
+      errorMessages: error?.response?.data?.errorMessages,
+    });
 
     return {
       error: {
