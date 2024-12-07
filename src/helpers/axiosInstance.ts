82ccd93b601f5be452590426df71153502ff4b79
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-12-07 11:26:25 +0600

    Add first solution code section about error from axios and with comment about why it did not work

--- src/helpers/axiosInstance.ts
+++ src/helpers/axiosInstance.ts
@@ -68,6 +68,14 @@ instance.interceptors.response.use(
     //   return responseObject;
     // }
 
+    // ! Below code is from ChatGPT solution it work but in receiving end response is not thrown into catch block instead in result also no message or statusCode property
+    // const responseObject: IGenericErrorResponse = {
+    //   statusCode: error?.response?.data?.statusCode || 500,
+    //   message: error?.response?.data?.message || "Something went wrong",
+    //   errorMessages: error?.response?.data?.errorMessages,
+    // };
+    // return responseObject;
+
     // ! I was trying to send this below object to promise.reject function but it did not work
     // console.log({
     //   success: error?.response?.data?.success || false,
