2eabfdb20a33ee9b2fd13dcbd1e1dc646894d490
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-12-20 10:49:07 +0600

    commented out getAllCars api end point auth headers

--- src/redux/api/carApi.ts
+++ src/redux/api/carApi.ts
@@ -18,6 +18,9 @@ const carApi = baseApi.injectEndpoints({
       query: () => ({
         url: CAR_URL,
         method: "GET",
+        // headers: {
+        //   Authorization: `Bearer ${localStorage.getItem(authKey)}`,
+        // },
       }),
       providesTags: ["car"],
     }),
