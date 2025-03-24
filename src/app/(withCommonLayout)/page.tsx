75d1e37870808ce9c29eb02ee79cf3357c8ea043
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-03-24 12:17:25 +0600

    Move HomePage folder to common folder in component

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,7 +1,7 @@
-import FeaturedCars from "@/components/UI/HomePage/FeaturedCars/FeaturedCars";
-import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
-import Testimonial from "@/components/UI/HomePage/Testimonial/Testimonial";
-import WhyUs from "@/components/UI/HomePage/WhyUs/WhyUs";
+import FeaturedCars from "@/components/Common/HomePage/FeaturedCars/FeaturedCars";
+import HeroSection from "@/components/Common/HomePage/HeroSection/HeroSection";
+import Testimonial from "@/components/Common/HomePage/Testimonial/Testimonial";
+import WhyUs from "@/components/Common/HomePage/WhyUs/WhyUs";
 
 export default function Home() {
   return (
