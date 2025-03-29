bf200c81451e75047e065f24198500a7edba8706
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-03-29 14:28:22 +0600

    Add metadata with title and description to TermOfServicePage

--- src/app/(withCommonLayout)/tos/page.tsx
+++ src/app/(withCommonLayout)/tos/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "Terms of Service - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const TermOfServicePage = () => {
   return (
