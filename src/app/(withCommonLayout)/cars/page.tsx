c69c3723e975aed502b52b366b49254a24e7182c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-02 10:55:22 +0600

    Add metadata with title and description to car page

--- src/app/(withCommonLayout)/cars/page.tsx
+++ src/app/(withCommonLayout)/cars/page.tsx
@@ -1,4 +1,10 @@
 import CarsPage from "@/components/Common/Car/CarsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Our Cars - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const Car = () => {
   return <CarsPage />;
