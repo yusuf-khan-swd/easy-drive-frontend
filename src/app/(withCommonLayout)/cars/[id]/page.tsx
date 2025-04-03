1f848e604e1d3171ed899e8ae5e7cfadef3c7e58
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-03 11:41:13 +0600

    Add metadata with title and description to CarDetails server component

--- src/app/(withCommonLayout)/cars/[id]/page.tsx
+++ src/app/(withCommonLayout)/cars/[id]/page.tsx
@@ -1,4 +1,10 @@
 import CarDetailsPage from "@/components/Common/Car/CarDetailsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Car Details - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const CarDetails = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
