adf453cc8cb76c96703e6b9e70819e2903542e0a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-06 10:21:05 +0600

    Add metadata with title and description to Contact server component

--- src/app/(withCommonLayout)/contact/page.tsx
+++ src/app/(withCommonLayout)/contact/page.tsx
@@ -1,4 +1,10 @@
 import ContactPage from "@/components/Common/ContactPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const Contact = () => {
   return <ContactPage />;
