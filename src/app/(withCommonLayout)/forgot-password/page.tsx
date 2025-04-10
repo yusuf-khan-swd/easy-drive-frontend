9e54786c91c72de90d3eb7ad0b2b2fdc82d74c02
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-10 10:12:00 +0600

    Add metadata with title and description to ForgotPassword server component

--- src/app/(withCommonLayout)/forgot-password/page.tsx
+++ src/app/(withCommonLayout)/forgot-password/page.tsx
@@ -1,4 +1,10 @@
 import ForgotPasswordPage from "@/components/Common/ForgotPasswordPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Forgot Password - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const ForgotPassword = () => {
   return <ForgotPasswordPage />;
