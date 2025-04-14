90f9cd74a8fb46a786068265fc6a58353171ff36
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-14 11:26:57 +0600

    Add metadata with title and description to PaymentStatus server component

--- src/app/(withCommonLayout)/payment/page.tsx
+++ src/app/(withCommonLayout)/payment/page.tsx
@@ -1,5 +1,10 @@
 import PaymentStatusPage from "@/components/Common/PaymentStatusPage";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Payment Status - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 interface PropTypes {
   searchParams: { status: string };
 }
