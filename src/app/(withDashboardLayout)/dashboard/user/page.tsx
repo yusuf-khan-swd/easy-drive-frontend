c6bc3e08633ae65fae776888f95174978dd1c09c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-16 12:28:10 +0600

    Add metadata with title and description to User server component

--- src/app/(withDashboardLayout)/dashboard/user/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import UserProfile from "./profile/page";
+
+export const metadata: Metadata = {
+  title: "User Profile - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const User = () => {
   return <UserProfile />;
