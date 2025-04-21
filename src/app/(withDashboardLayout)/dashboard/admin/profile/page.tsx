2890ff8f94c6682ae21adebc1a8c6abd820df07f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-21 09:17:58 +0600

    clean AdminProfile server page by removing extra div wrapper for ProfilePage component

--- src/app/(withDashboardLayout)/dashboard/admin/profile/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/profile/page.tsx
@@ -1,11 +1,7 @@
 import ProfilePage from "@/components/Dashboard/ProfilePage/ProfilePage";
 
 const AdminProfile = () => {
-  return (
-    <div>
-      <ProfilePage />
-    </div>
-  );
+  return <ProfilePage />;
 };
 
 export default AdminProfile;
