9aa74d0d650e576a9467d18879e73ab92ef6e2f4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-27 09:24:41 +0600

    Add metadata with title and description to ManageReviews server component

--- src/app/(withDashboardLayout)/dashboard/user/manage-reviews/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-reviews/page.tsx
@@ -1,4 +1,10 @@
 import ManageReviewsPage from "@/components/Dashboard/ManageReviews/ManageReviewsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Reviews - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const ManageReviews = () => {
   return <ManageReviewsPage />;
