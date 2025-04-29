69f062a2720682116191f7eef9236bb951665e53
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-04-29 10:38:20 +0600

    Get review id from params and pass it to UpdateReviewPage client component

--- src/app/(withDashboardLayout)/dashboard/user/manage-reviews/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-reviews/edit/[id]/page.tsx
@@ -1,5 +1,13 @@
-const UpdateReview = () => {
-  return <div></div>;
+import UpdateReviewPage from "@/components/Dashboard/ManageReviews/UpdateReviewPage";
+
+const UpdateReview = ({ params }: { params: { id: string } }) => {
+  const id = params?.id;
+
+  return (
+    <div>
+      <UpdateReviewPage id={id} />
+    </div>
+  );
 };
 
 export default UpdateReview;
