aa5773f46dd13ce95c81a55b647b15ed51cdbf77
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-01 11:00:55 +0600

    Add metadata with title and description to UpdateReview server component

--- src/app/(withDashboardLayout)/dashboard/user/manage-reviews/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-reviews/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import UpdateReviewPage from "@/components/Dashboard/ManageReviews/UpdateReviewPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update Reviews - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const UpdateReview = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
