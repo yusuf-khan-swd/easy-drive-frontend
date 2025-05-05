740b9785e0d0c4a50b17c514d0c717cea3a28e82
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-05 10:00:07 +0600

    Add metadata with title and description to ManagePayments server component

--- src/app/(withDashboardLayout)/dashboard/user/manage-payments/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-payments/page.tsx
@@ -1,4 +1,10 @@
 import ManagePaymentsPage from "@/components/Dashboard/ManagePayments/ManagePaymentsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Payments - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const ManagePayments = () => {
   return <ManagePaymentsPage />;
