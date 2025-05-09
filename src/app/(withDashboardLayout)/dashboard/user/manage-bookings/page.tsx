87230d8df9cc7836e462651c0c24612abddefcd5
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-10 02:41:07 +0600

    remove extra div wrapper from ManageBookingsPage component in ManageBookings

--- src/app/(withDashboardLayout)/dashboard/user/manage-bookings/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-bookings/page.tsx
@@ -1,11 +1,7 @@
 import ManageBookingsPage from "@/components/Dashboard/ManageBookings/ManageBookings";
 
 const ManageBookings = () => {
-  return (
-    <div>
-      <ManageBookingsPage />
-    </div>
-  );
+  return <ManageBookingsPage />;
 };
 
 export default ManageBookings;
