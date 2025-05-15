b7a99b52c1b3a5ddb0003f8f02aa71075c37be2a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-15 10:40:09 +0600

    Remove extra div wrapper in ReturnCarPage

--- src/app/(withDashboardLayout)/dashboard/user/manage-bookings/return/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-bookings/return/[id]/page.tsx
@@ -3,11 +3,7 @@ import ReturnCarPage from "@/components/Dashboard/ReturnCar/ReturnCarPage";
 const ReturnCar = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
 
-  return (
-    <div>
-      <ReturnCarPage id={id} />
-    </div>
-  );
+  return <ReturnCarPage id={id} />;
 };
 
 export default ReturnCar;
