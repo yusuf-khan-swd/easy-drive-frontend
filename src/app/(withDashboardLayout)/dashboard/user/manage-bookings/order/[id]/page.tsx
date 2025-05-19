7634a82e2c823d2408f242172b8393e11d5b93d9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-19 10:08:43 +0600

    Remove extra div wrapper of OrderPage

--- src/app/(withDashboardLayout)/dashboard/user/manage-bookings/order/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-bookings/order/[id]/page.tsx
@@ -3,11 +3,7 @@ import OrderPage from "@/components/Dashboard/Order/OrderPage";
 const Order = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
 
-  return (
-    <div>
-      <OrderPage id={id} />
-    </div>
-  );
+  return <OrderPage id={id} />;
 };
 
 export default Order;
