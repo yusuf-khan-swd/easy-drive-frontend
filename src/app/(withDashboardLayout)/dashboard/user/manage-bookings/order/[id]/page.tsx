a114b0f73cfa10a4746505a838777b2cf73e2792
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-20 10:01:44 +0600

    Add metadata with title and description to Order server component

--- src/app/(withDashboardLayout)/dashboard/user/manage-bookings/order/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-bookings/order/[id]/page.tsx
@@ -1,4 +1,10 @@
 import OrderPage from "@/components/Dashboard/Order/OrderPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Car Order - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const Order = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
