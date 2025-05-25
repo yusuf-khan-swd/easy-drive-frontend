5df299169a52d5ca1adbbf7e6cb32077c7dc2a57
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-25 11:05:41 +0600

    Add metadata with title and description to UpdateBooking server component

--- src/app/(withDashboardLayout)/dashboard/user/manage-bookings/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-bookings/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import UpdateBookingPage from "@/components/Dashboard/ManageBookings/UpdateBookingPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update Booking - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const UpdateBooking = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
