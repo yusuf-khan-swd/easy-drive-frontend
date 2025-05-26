446b72653303f8e1af2b9bc23e80d984e6ac6bea
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-26 09:01:47 +0600

    Refactor Booking component by removing client code

--- src/app/(withDashboardLayout)/dashboard/user/booking/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/booking/[id]/page.tsx
@@ -1,35 +1,5 @@
-"use client";
-
-import BookingForm from "@/components/Dashboard/Booking/BookingForm";
-import LoadingSpinner from "@/components/Shared/LoadingSpinner";
-import CarCard from "@/components/UI/Car/CarCard";
-import { useGetSingleCarQuery } from "@/redux/api/carApi";
-
-const Booking = ({ params }: { params: { id: string } }) => {
-  const id = params?.id;
-
-  const { data, isLoading } = useGetSingleCarQuery(id || "");
-  const car = data?.data;
-
-  if (isLoading) return <LoadingSpinner />;
-
-  return (
-    <div>
-      <h2 className="text-2xl font-bold text-center mb-6">Booking Details</h2>
-      {car ? (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-          {/* Form for selecting date and time */}
-          <BookingForm carId={car?._id} />
-
-          <div className="grid grid-cols-1 flex-grow">
-            <CarCard car={car} bookingPage />
-          </div>
-        </div>
-      ) : (
-        <h3 className="text-2xl font-bold text-center">No data available</h3>
-      )}
-    </div>
-  );
+const Booking = () => {
+  return <div></div>;
 };
 
 export default Booking;
