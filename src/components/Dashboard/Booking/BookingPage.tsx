e9e5866b4f41df4deeb4824b90bb014866f038cb
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-05-26 09:02:37 +0600

    Implement Booking client code to BookingPage component

--- src/components/Dashboard/Booking/BookingPage.tsx
+++ src/components/Dashboard/Booking/BookingPage.tsx
@@ -1,5 +1,35 @@
-const BookingPage = () => {
-  return <div></div>;
+"use client";
+
+import BookingForm from "@/components/Dashboard/Booking/BookingForm";
+import LoadingSpinner from "@/components/Shared/LoadingSpinner";
+import CarCard from "@/components/UI/Car/CarCard";
+import { useGetSingleCarQuery } from "@/redux/api/carApi";
+
+const BookingPage = ({ params }: { params: { id: string } }) => {
+  const id = params?.id;
+
+  const { data, isLoading } = useGetSingleCarQuery(id || "");
+  const car = data?.data;
+
+  if (isLoading) return <LoadingSpinner />;
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-center mb-6">Booking Details</h2>
+      {car ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Form for selecting date and time */}
+          <BookingForm carId={car?._id} />
+
+          <div className="grid grid-cols-1 flex-grow">
+            <CarCard car={car} bookingPage />
+          </div>
+        </div>
+      ) : (
+        <h3 className="text-2xl font-bold text-center">No data available</h3>
+      )}
+    </div>
+  );
 };
 
 export default BookingPage;
