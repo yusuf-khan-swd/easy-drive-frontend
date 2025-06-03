cb91233f837388a38021fbf800420a9a6344109c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-06-03 11:08:20 +0600

    Add metadata with title and description to ChangePassword server component

--- src/app/(withDashboardLayout)/dashboard/change-password/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/change-password/page.tsx
@@ -1,4 +1,10 @@
 import ChangePasswordPage from "@/components/Dashboard/ChangePassword/ChangePasswordPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Change Password - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const ChangePassword = () => {
   return <ChangePasswordPage />;
