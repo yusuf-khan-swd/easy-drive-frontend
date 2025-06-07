3a9dce61f91f89b2c7603327017229de3c126962
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-06-07 12:50:28 +0600

    Add metadata with title and description to ManageUsers server component

--- src/app/(withDashboardLayout)/dashboard/admin/manage-users/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-users/page.tsx
@@ -1,4 +1,10 @@
 import ManageUsersPage from "@/components/Dashboard/ManageUsers/ManageUsersPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Users - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const ManageUsers = () => {
   return <ManageUsersPage />;
