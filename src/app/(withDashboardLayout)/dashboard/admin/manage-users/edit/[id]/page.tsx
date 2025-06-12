7da43b58136118b088eda096df4d9a1de96ad500
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-06-12 10:18:41 +0600

    Add metadata with title and description to UpdateUser server component

--- src/app/(withDashboardLayout)/dashboard/admin/manage-users/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-users/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import UpdateUserPage from "@/components/Dashboard/ManageUsers/UpdateUserPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update User - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const UpdateUser = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
