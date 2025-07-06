9a82c78821429c10047d8ff26fb3a51f90b3942f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-07-06 11:23:17 +0600

    Add metadata with title and description to CreateAdmin server component

--- src/app/(withDashboardLayout)/dashboard/admin/manage-users/create-admin/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-users/create-admin/page.tsx
@@ -1,4 +1,10 @@
 import CreateAdminPage from "@/components/Dashboard/ManageUsers/CreateAdminPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Admin - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const CreateAdmin = () => {
   return <CreateAdminPage />;
