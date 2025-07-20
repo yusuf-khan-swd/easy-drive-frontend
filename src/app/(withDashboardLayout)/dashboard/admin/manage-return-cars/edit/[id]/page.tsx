6e35e7344c91c790fb341e6e7bd4f19ab09b8cb1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-07-20 11:30:34 +0600

    Add metadata with title and description to AdminReturnCarPage server component

--- src/app/(withDashboardLayout)/dashboard/admin/manage-return-cars/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-return-cars/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import AdminReturnCarPage from "@/components/Dashboard/ManageReturnCar/AdminReturnCarPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Return Cars - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const AdminReturnCar = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
