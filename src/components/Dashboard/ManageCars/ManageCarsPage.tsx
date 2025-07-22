57184a3c40b1cbbaefcf3e21006754f49e7783eb
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-07-22 10:54:02 +0600

    Add client code for Manage Cars to ManageCarsPage component

--- src/components/Dashboard/ManageCars/ManageCarsPage.tsx
+++ src/components/Dashboard/ManageCars/ManageCarsPage.tsx
@@ -1,5 +1,109 @@
+"use client";
+
+import { useDeleteCarMutation, useGetAllCarsQuery } from "@/redux/api/carApi";
+import { useDebounced } from "@/redux/hooks";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Box, Button, IconButton, Stack, TextField } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
+
 const ManageCarsPage = () => {
-  return <div></div>;
+  const query: Record<string, any> = {};
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  // console.log(searchTerm);
+
+  const debouncedTerm = useDebounced({
+    searchQuery: searchTerm,
+    delay: 600,
+  });
+
+  if (!!debouncedTerm) {
+    query["searchTerm"] = searchTerm;
+  }
+
+  const { data, isLoading, isError } = useGetAllCarsQuery("");
+  const [deleteCar] = useDeleteCarMutation();
+
+  const cars = data?.data;
+
+  const handleDelete = async (id: string) => {
+    try {
+      const result = await deleteCar(id).unwrap();
+      toast.success(result?.message || "Car deleted Successfully");
+    } catch (error: any) {
+      console.log("Error: ", error);
+      toast.error(
+        error?.data?.message || error?.data?.data || "Car delete failed"
+      );
+    }
+  };
+
+  const columns: GridColDef[] = [
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "description", headerName: "Description", flex: 3 },
+    { field: "pricePerHour", headerName: "PricePerHour", flex: 1 },
+    {
+      field: "action",
+      headerName: "Action",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <IconButton
+              onClick={() => handleDelete(row._id)}
+              aria-label="delete"
+            >
+              <DeleteIcon sx={{ color: "red" }} />
+            </IconButton>
+            <Link href={`/dashboard/admin/manage-cars/edit/${row._id}`}>
+              <IconButton aria-label="edit">
+                <EditIcon />
+              </IconButton>
+            </Link>
+          </Box>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        {/* TODO: See can modal can be add for create car */}
+        <Link href="/dashboard/admin/manage-cars/create">
+          <Button size="small">Create</Button>
+        </Link>
+        <TextField
+          onChange={(e) => setSearchTerm(e.target.value)}
+          size="small"
+          placeholder="search cars"
+        />
+      </Stack>
+      {isError ? (
+        <h2>No Data Available</h2>
+      ) : (
+        <Box sx={{ my: 2, minWidth: "840px" }}>
+          <DataGrid
+            rows={cars}
+            columns={columns}
+            getRowId={(row) => row._id}
+            loading={isLoading}
+            slotProps={{
+              loadingOverlay: {
+                variant: "linear-progress",
+                noRowsVariant: "skeleton",
+              },
+            }}
+          />
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default ManageCarsPage;
