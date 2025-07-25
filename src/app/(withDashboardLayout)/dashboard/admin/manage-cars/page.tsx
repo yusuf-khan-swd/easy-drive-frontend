45edb6e6055781a1b34b0959ba09430b1d56bdc7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-07-25 11:39:22 +0600

    Add metadata with title and description to ManageCars server component

--- src/app/(withDashboardLayout)/dashboard/admin/manage-cars/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-cars/page.tsx
@@ -1,4 +1,10 @@
 import ManageCarsPage from "@/components/Dashboard/ManageCars/ManageCarsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Cars - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const ManageCars = () => {
   return <ManageCarsPage />;
