92a4927cf7b450d669146b47bd2adb3b84f310e1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-07-30 11:57:20 +0600

    Add metadata with title and description to CreateCar server component

--- src/app/(withDashboardLayout)/dashboard/admin/manage-cars/create/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-cars/create/page.tsx
@@ -1,4 +1,10 @@
 import CreateCarPage from "@/components/Dashboard/ManageCars/CreateCarPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Create Car - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const CreateCar = () => {
   return <CreateCarPage />;
