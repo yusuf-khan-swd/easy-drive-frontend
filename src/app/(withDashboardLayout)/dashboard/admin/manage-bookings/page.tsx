7981aa48012cdff89bb3fa5a0c44c86d123a3f37
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-05 11:48:22 +0600

    Add metadata with title and description to AdminManageBookings server component

--- src/app/(withDashboardLayout)/dashboard/admin/manage-bookings/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-bookings/page.tsx
@@ -1,4 +1,10 @@
 import AdminManageBookingsPage from "@/components/Dashboard/ManageBookings/AdminManageBookingsPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Manage Bookings - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const AdminManageBookings = () => {
   return <AdminManageBookingsPage />;
