0f059a5e3e71fd4fabd2147054aaa4ae7d4741ed
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-07 11:51:57 +0600

    Add update booking client code to AdminUpdateBookingPage client component

--- src/components/Dashboard/ManageBookings/AdminUpdateBookingPage.tsx
+++ src/components/Dashboard/ManageBookings/AdminUpdateBookingPage.tsx
@@ -1,5 +1,36 @@
-const AdminUpdateBookingPage = () => {
-  return <div></div>;
+"use client";
+
+import UpdateBookingForm from "@/components/Dashboard/ManageBookings/UpdateBookingForm";
+import LoadingSpinner from "@/components/Shared/LoadingSpinner";
+import CarCard from "@/components/UI/Car/CarCard";
+import { useGetSingleBookingQuery } from "@/redux/api/bookingApi";
+
+const AdminUpdateBookingPage = ({ params }: { params: { id: string } }) => {
+  const id = params?.id;
+
+  const { data, isLoading } = useGetSingleBookingQuery(id || "");
+
+  const booking = data?.data;
+  const car = booking?.car;
+
+  if (isLoading) return <LoadingSpinner />;
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-center mb-6">Booking Details</h2>
+      {booking ? (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          {/* Form for selecting date and time */}
+          <UpdateBookingForm booking={booking} />
+          <div className="grid">
+            <CarCard car={car} bookingPage />
+          </div>
+        </div>
+      ) : (
+        <h3 className="text-2xl font-bold text-center">No data available</h3>
+      )}
+    </div>
+  );
 };
 
 export default AdminUpdateBookingPage;
