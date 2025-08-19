108d2ef441d14d9e7f87d7dab7b89960ee74feaf
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-19 10:24:39 +0600

    Use WEBSITE_NAME constant for meta data title and WEBSITE_DESCRIPTION constant for meta data description

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,4 +1,6 @@
+import { WEBSITE_DESCRIPTION, WEBSITE_NAME } from "./global";
+
 export const METADATA_FOR_MAIN_LAYOUT = {
-  title: "EasyDrive",
-  description: "A Car Rental Service Provider",
+  title: WEBSITE_NAME,
+  description: WEBSITE_DESCRIPTION,
 };
