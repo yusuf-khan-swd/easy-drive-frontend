d255586bbc708dcd4c07ec573f108c83623ce2a7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-20 13:22:49 +0600

    Uses shared metadata constant for home page title

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,8 +1,9 @@
 import HomePage from "@/components/Common/HomePage/HomePage";
+import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home - EasyDrive",
+  title: `Home - ${METADATA_FOR_MAIN_LAYOUT.title}`,
   description: "A Car Rental Service Provider",
 };
 
