ba87729125bdade0bacc1d5975e34fa73ef293c0
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-21 10:38:59 +0600

    use variable to store metadataTitle and metadataDescription

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,6 +1,9 @@
 import { WEBSITE_DESCRIPTION, WEBSITE_NAME } from "./global";
 
+const metadataTitle = WEBSITE_NAME;
+const metadataDescription = WEBSITE_DESCRIPTION;
+
 export const METADATA_FOR_MAIN_LAYOUT = {
-  title: WEBSITE_NAME,
-  description: WEBSITE_DESCRIPTION,
+  title: metadataTitle,
+  description: metadataDescription,
 };
