2059ff2581e0c9259c93a1260b438d8ed01dff75
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-22 11:23:34 +0600

    Back to raw string as title for home page metadata

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,8 @@
 import HomePage from "@/components/Common/HomePage/HomePage";
-import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: `Home - ${METADATA_FOR_MAIN_LAYOUT.title}`,
+  title: "Home - EasyDrive",
   description: "A Car Rental Service Provider",
 };
 
