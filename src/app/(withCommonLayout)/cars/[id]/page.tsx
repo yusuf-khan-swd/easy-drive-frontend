83eaf910466a31d512329e10cf8405151ed532e8
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-25 11:11:50 +0600

    Uses shared metadata description for CarDetails

--- src/app/(withCommonLayout)/cars/[id]/page.tsx
+++ src/app/(withCommonLayout)/cars/[id]/page.tsx
@@ -1,9 +1,10 @@
 import CarDetailsPage from "@/components/Common/Car/CarDetailsPage";
+import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Car Details - EasyDrive",
-  description: "A Car Rental Service Provider",
+  description: METADATA_FOR_MAIN_LAYOUT.description,
 };
 
 const CarDetails = ({ params }: { params: { id: string } }) => {
