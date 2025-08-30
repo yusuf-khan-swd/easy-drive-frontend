239259ac24240c0d115694041c3313044970171c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-08-30 10:06:55 +0600

    Uses shared metadata description for PaymentStatus

--- src/app/(withCommonLayout)/payment/page.tsx
+++ src/app/(withCommonLayout)/payment/page.tsx
@@ -1,9 +1,10 @@
 import PaymentStatusPage from "@/components/Common/PaymentStatusPage";
+import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Payment Status - EasyDrive",
-  description: "A Car Rental Service Provider",
+  description: METADATA_FOR_MAIN_LAYOUT.description,
 };
 interface PropTypes {
   searchParams: { status: string };
