222591611e4ee4e8b8e460f7869c41aeacdbcc4d
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-09-07 10:15:06 +0600

    Uses shared metadata for login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,11 @@
 import LoginPage from "@/components/Common/Logins/LoginPage";
+import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login - EasyDrive",
+  description: METADATA_FOR_MAIN_LAYOUT.description,
+};
 
 const Login = () => {
   return <LoginPage />;
