abaa688e05a48b8285b7419771498ff2c7ad0d1c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-09-12 08:25:02 +0600

    Uses metadata for register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,14 @@
 import RegisterPage from "@/components/Common/Logins/RegisterPage";
+import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Register - EasyDrive",
+  description: METADATA_FOR_MAIN_LAYOUT.description,
+};
 
 const Register = () => {
-  return <RegisterPage/>;
+  return <RegisterPage />;
 };
 
 export default Register;
