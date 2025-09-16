849cb932c015400280b6329a6d85df3831705d13
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-09-16 12:36:38 +0600

    Uses shared metadata for ChangePassword page

--- src/app/(withDashboardLayout)/dashboard/change-password/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/change-password/page.tsx
@@ -1,9 +1,10 @@
 import ChangePasswordPage from "@/components/Dashboard/ChangePassword/ChangePasswordPage";
+import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Change Password - EasyDrive",
-  description: "A Car Rental Service Provider",
+  description: METADATA_FOR_MAIN_LAYOUT.description,
 };
 
 const ChangePassword = () => {
