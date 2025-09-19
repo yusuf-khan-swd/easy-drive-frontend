9399bec712608c24e8bc132648cc1a25c449f799
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-09-19 10:12:12 +0600

    Add metadata to DashboardHome component

--- src/app/(withDashboardLayout)/dashboard/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/page.tsx
@@ -1,5 +1,11 @@
 import DashboardDrawer from "@/components/Dashboard/DashboardDrawer";
 import ProfilePage from "@/components/Dashboard/ProfilePage/ProfilePage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const DashboardHome = () => {
   return (
