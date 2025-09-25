f38e10cb96cf62db3e8969b0ecbc680ba4298c31
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-09-25 11:43:05 +0600

    Uses shared metadata title for ChangePassword page

--- src/app/(withDashboardLayout)/dashboard/change-password/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/change-password/page.tsx
@@ -3,7 +3,7 @@ import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Change Password - EasyDrive",
+  title: `Change Password - ${METADATA_FOR_MAIN_LAYOUT.title}`,
   description: METADATA_FOR_MAIN_LAYOUT.description,
 };
 
