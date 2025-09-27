c33aedf24ab8d26e6eee30d1093b855612ecabb4
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-09-27 09:05:46 +0600

    Uses shared metadata title for Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login - EasyDrive",
+  title: `Login - ${METADATA_FOR_MAIN_LAYOUT.title}`,
   description: METADATA_FOR_MAIN_LAYOUT.description,
 };
 
