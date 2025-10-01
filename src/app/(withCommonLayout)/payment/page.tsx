669fdd329a6610953be4fab09010ca181bb3be79
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-10-01 11:06:15 +0600

    Uses shared metadata title for PaymentStatus page

--- src/app/(withCommonLayout)/payment/page.tsx
+++ src/app/(withCommonLayout)/payment/page.tsx
@@ -3,7 +3,7 @@ import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Payment Status - EasyDrive",
+  title: `Payment Status - ${METADATA_FOR_MAIN_LAYOUT.title}`,
   description: METADATA_FOR_MAIN_LAYOUT.description,
 };
 interface PropTypes {
