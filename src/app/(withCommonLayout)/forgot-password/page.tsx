c1f1a8131f7d6c7e45973ba1a8a9feb45baf0c2c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-10-03 11:24:15 +0600

    Uses shared metadata title for ForgotPassword server page

--- src/app/(withCommonLayout)/forgot-password/page.tsx
+++ src/app/(withCommonLayout)/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import { METADATA_FOR_MAIN_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Forgot Password - EasyDrive",
+  title: `Forgot Password - ${METADATA_FOR_MAIN_LAYOUT.title}`,
   description: METADATA_FOR_MAIN_LAYOUT.description,
 };
 
