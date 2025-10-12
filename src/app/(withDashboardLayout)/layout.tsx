84be84d58364ecd4510f47d31039b0836fa788ef
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-10-12 11:29:26 +0600

    Add todo for DashboardLayout

--- src/app/(withDashboardLayout)/layout.tsx
+++ src/app/(withDashboardLayout)/layout.tsx
@@ -4,6 +4,7 @@ import { isLoggedIn } from "@/services/auth.service";
 import { useRouter } from "next/navigation";
 
 // ! After user logged out then token is remove but user is not redirect to login page but unauthorize user can still in dashboard but no data will be shown
+// TODO: So have check this above issue out because if user click on logout button it should redirect to '/' root page from Navbar component
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
 
