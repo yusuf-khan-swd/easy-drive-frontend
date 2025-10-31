0e5631dc906e93a8c043d9240b3c1553b18a9ae9
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-10-31 12:15:03 +0600

    Uses shared dashboard metadata title for UpdateReview server page

--- src/app/(withDashboardLayout)/dashboard/user/manage-reviews/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-reviews/edit/[id]/page.tsx
@@ -1,8 +1,9 @@
 import UpdateReviewPage from "@/components/Dashboard/ManageReviews/UpdateReviewPage";
+import { METADATA_FOR_DASHBOARD_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Update Reviews - EasyDrive",
+  title: `Update Reviews - ${METADATA_FOR_DASHBOARD_LAYOUT.title}`,
   description: "A Car Rental Service Provider",
 };
 
