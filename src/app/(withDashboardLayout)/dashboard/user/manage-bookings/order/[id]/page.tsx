51ddee6299208a55563e352a8ab2167373b61644
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-11-08 11:40:15 +0600

    Uses shared dashboard metadata title for Order server page

--- src/app/(withDashboardLayout)/dashboard/user/manage-bookings/order/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/manage-bookings/order/[id]/page.tsx
@@ -1,8 +1,9 @@
 import OrderPage from "@/components/Dashboard/Order/OrderPage";
+import { METADATA_FOR_DASHBOARD_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Car Order - EasyDrive",
+  title: `Car Order - ${METADATA_FOR_DASHBOARD_LAYOUT.title}`,
   description: "A Car Rental Service Provider",
 };
 
