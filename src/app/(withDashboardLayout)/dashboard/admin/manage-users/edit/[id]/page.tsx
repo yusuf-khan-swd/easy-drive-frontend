759497bcd6b1c7c1afeeb760b481422561f0264f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-11-16 13:03:02 +0600

    Uses shared dashboard metadata title for UpdateUser server page

--- src/app/(withDashboardLayout)/dashboard/admin/manage-users/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-users/edit/[id]/page.tsx
@@ -1,8 +1,9 @@
 import UpdateUserPage from "@/components/Dashboard/ManageUsers/UpdateUserPage";
+import { METADATA_FOR_DASHBOARD_LAYOUT } from "@/constants/metadata";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Update User - EasyDrive",
+  title: `Update User - ${METADATA_FOR_DASHBOARD_LAYOUT.title}`,
   description: "A Car Rental Service Provider",
 };
 
