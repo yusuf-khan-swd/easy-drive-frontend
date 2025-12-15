4c9bbcb04323b968fb10bb3dc63c5d4fb9f8e21a
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2025-12-15 09:33:59 +0600

    Add metadata to UpdateCar server page

--- src/app/(withDashboardLayout)/dashboard/admin/manage-cars/edit/[id]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/manage-cars/edit/[id]/page.tsx
@@ -1,4 +1,10 @@
 import UpdateCarPage from "@/components/Dashboard/ManageCars/UpdateCarPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Update Car - EasyDrive",
+  description: "A Car Rental Service Provider",
+};
 
 const UpdateCar = ({ params }: { params: { id: string } }) => {
   const id = params?.id;
