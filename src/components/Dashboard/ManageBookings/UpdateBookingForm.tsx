392af74802b11a57c1e805adbfa6f671a2743f75
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2026-01-12 09:53:54 +0600

    use .trim() method for startTime in UpdateBookingForm component

--- src/components/Dashboard/ManageBookings/UpdateBookingForm.tsx
+++ src/components/Dashboard/ManageBookings/UpdateBookingForm.tsx
@@ -30,7 +30,7 @@ const UpdateBookingForm = ({ booking }: { booking: any }) => {
         const updateBookingData = {
           _id: booking?._id,
           date,
-          startTime,
+          startTime: startTime.trim(),
         };
 
         const result = await updateBooking(updateBookingData).unwrap();
